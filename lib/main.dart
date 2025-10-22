dbfa3aabf375bd500d83deeadd163ba89e0fcede
PovDavin <davin.pov@student.cadt.edu.kh>
2025-10-23 00:12:47 +0700

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'ui/quiz_console.dart';
 import 'data/quiz_file_provider.dart';
 
 void main() {
-  var filePath = 'assets/quiz.json';
 
+  var filePath = 'assets/quiz.json';
   QuizRepository repo = QuizRepository(filePath);
   Quiz quiz = repo.readQuiz();
 
   QuizConsole console = QuizConsole(quiz: quiz);
-
   console.startQuiz();
 
   var filePathWrite = 'assets/quiz-input.json';
